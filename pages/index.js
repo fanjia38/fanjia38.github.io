07ac1419ba5ce6da2cd5100fec206fc0e0ad0914
fanjia38 <38fanjia@gmail.com>
2021-01-29 16:57:27 +0900

    :up: update package - @emotion/core 10.0.1 => 11.0.0 - @emotion/styled 10.0.27 => 11.0.0 - babel-plugin-emotion 10.0.33 => 11.0.0 - next 10.0.1 => 10.0.5

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../src/components/layout'
+import About from '../src/components/About'
+import Contents from '../src/components/contents'
+
+function Index() {
+  return (
+    <Layout>
+      <About/>
+      <Contents />
+    </Layout>
+  )
+}
+export default Index
