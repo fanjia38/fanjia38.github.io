9930fc3cec0d046cbd26e89c16fde9398d5c08af
fanjia38 <38fanjia@gmail.com>
2021-02-01 21:18:55 +0900

    :+1: update skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -6,7 +6,7 @@ const frontendData = {
   datasets: [
     {
       label: 'Front-End',
-      data: [2, 9, 3, 5, 2, 3],
+      data: [3, 4, 4, 4, 3, 3],
       backgroundColor: 'rgba(255, 208, 127, 0.5)',
       borderColor: '#ffa500',
       borderWidth: 1,
@@ -15,11 +15,11 @@ const frontendData = {
 }
 
 const backendData = {
-  labels: ['PHP', 'Symfony', 'FuelPHP', 'SQL', 'Node.js', 'Perl'],
+  labels: ['PHP', 'SQL', 'Node.js', 'Perl', 'Shellscript'],
   datasets: [
     {
-      label: 'Front-End',
-      data: [2, 9, 3, 5, 2, 2],
+      label: 'Back-End',
+      data: [3, 3, 4, 2, 2],
       backgroundColor: 'rgba(104, 150, 79, 0.5)',
       borderColor: '#589738',
       borderWidth: 1,
@@ -41,7 +41,7 @@ function Skills() {
       <Container m="4">
         <Box>
           <Heading size="sm">Front-End</Heading>
-          <Text fontSize="sm">JavaScript / TypeScript / React / Redux / Next.js / Styled Components / Webpack / Jest / HTML / CSS(Sass) / jQuery / Knockout.js</Text>
+          <Text fontSize="sm">JavaScript / TypeScript / React(Hooks) / Redux / Styled Components / Webpack / Jest / HTML / CSS(Sass) / jQuery / Knockout.js</Text>
           <Text fontSize="sm">フロントエンドエンジニアとして経験3年。直近では React + Redux + TypeScript を使用。</Text>
           <Radar type="rader" data={frontendData} options={options} />
         </Box>
@@ -50,6 +50,15 @@ function Skills() {
           <Text fontSize="sm">PHP(Symfony, FuelPHP, CakePHP) / SQL / Node.js / Perl</Text>
           <Text fontSize="sm">バックエンドエンジニアとして経験3年。</Text>
           <Radar type="rader" data={backendData} options={options} />
+        </Box>
+        <Box marginTop="8" borderWidth="1px" borderColor="silver" borderRadius="4px">
+          <Text m="2" fontSize="sm">
+            1 ... 未経験<br />
+            2 ... 他者の協力を得ながら開発ができる<br />
+            3 ... 1人で開発を進められる<br />
+            4 ... 他者に教えることができる<br />
+            5 ... コミッター・開発者レベル
+          </Text>
         </Box>
       </Container>
     </Box>
